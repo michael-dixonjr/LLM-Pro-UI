9a11a739e9497070b0301a5b611639041c92bd61
Michael Dixon Jr <112347876+michael-dixonjr@users.noreply.github.com>
2023-08-09 15:25:41 -0400

    env variable test 2

--- server/server.js
+++ server/server.js
@@ -28,7 +28,8 @@ const textToSpeechClient = new TextToSpeechClient({
     auth_uri: process.env.AUTH_URI,
     token_uri: process.env.TOKEN_URI,
     auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+    universe_domain: process.env.UNIVERSE_DOMAIN
   }
 });
 
